4ad8551a472483c02e12d38601fb7d8b8b5c607e
dhiauddin02 <dhiauddin242@gmail.com>
2022-09-26 16:44:48 +0700

    menambahkan folder praktikum2 tentan struktur kontrol

--- praktikum1/dasar2.php
+++ praktikum1/dasar2.php
@@ -1,5 +1,4 @@
 <?php
 $alamat = "yagyakarta";
-echo $alamat; //program akan jalan juka variable nya dipanggil sesuai huruf besar kecilnya
-echo $Alamat; //program tidak jalan sebab variabenya huruf besar kecil tidak sesuai
+echo $Alamat;
 ?>
---
+++ teori/bootstrap/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+    <style>
+        button {
+            background-color: #0E5CE6;
+            border: none;
+            color: white;
+            height: 40px;
+            border-radius: 4px;
+
+        }
+
+        div {
+            background-color: darkgreen;
+            border: none;
+            color: white;
+            height: 40px;
+            width: 100px;
+            border-radius: 4px;
+
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <button>Tombol Coba 1</button> <br> <br>
+    <div>Tombol Coba 2</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+
+</html>
