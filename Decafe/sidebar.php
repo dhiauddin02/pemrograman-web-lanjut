a95bdd76eb829f8cb45f1834d876c95890001365
dhiauddin02 <67725949+dhiauddin02@users.noreply.github.com>
2022-11-13 21:12:24 +0700

    membuat route acces

--- Decafe/sidebar.php
+++ Decafe/sidebar.php
@@ -15,7 +15,7 @@
 
                          <li>
 
-                             <a href="index.php?x=dashboard" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "dashboard") ? "active link-light" : "link-dark" ?>">
+                             <a href="dashboard" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "dashboard") ? "active link-light" : "link-dark" ?>">
                                  <i class="bi bi-speedometer2 me-2"></i>
                                  Dashboard
                              </a>
@@ -23,25 +23,25 @@
 
                          </li>
                          <li>
-                             <a href="index.php?x=orders" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "orders") ? "active link-light" : "link-dark" ?>">
+                             <a href="orders" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "orders") ? "active link-light" : "link-dark" ?>">
                                  <i class="bi bi-cart4 me-2"></i>
                                  Orders
                              </a>
                          </li>
                          <li>
-                             <a href="index.php?x=products" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "products") ? "active link-light" : "link-dark" ?>">
+                             <a href="products" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "products") ? "active link-light" : "link-dark" ?>">
                                  <i class="bi bi-table me-2"></i>
                                  Products
                              </a>
                          </li>
                          <li>
-                             <a href="index.php?x=customers" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "customers") ? "active link-light" : "link-dark" ?>">
+                             <a href="customers" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "customers") ? "active link-light" : "link-dark" ?>">
                                  <i class="bi bi-person-circle me-2"></i>
                                  Customers
                              </a>
                          </li>
                          <li>
-                             <a href="index.php?x=reports" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "reports") ? "active link-light" : "link-dark" ?>">
+                             <a href="reports" class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == "reports") ? "active link-light" : "link-dark" ?>">
                                  <i class="bi bi-envelope-paper me-2"></i>
                                  Reports
                              </a>
